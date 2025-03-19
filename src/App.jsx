80d5177c267f4f61fd1b626d8f6f3b18aed5bf8c
shiva2318 <shivasharath368@gmail.com>
2025-03-19 10:21:16 +0530

    created pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp'; // Fix: Corrected component name
+import About from  './pages/About';
+import Profile from './pages/Profile'; // Fix: Ensure consistent casing
 
 export default function App() {
-  return <h1 className='text-red-500'>APP</h1>
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/sign-in' element={<SignIn />} />
+                <Route path='/sign-up' element={<SignUp />} /> {/* Fixed typo */}
+                <Route path='/about' element={<About />} />
+                <Route path='/profile' element={<Profile />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
+
+
+
