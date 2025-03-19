8bbdc44927c3f393e741db9b75de1833f2de1e86
shiva2318 <shivasharath368@gmail.com>
2025-03-19 23:01:40 +0530

    create and run the server

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp'; // Fix: Corrected component name
+import SignUp from './pages/SignUp'; 
 import About from  './pages/About';
-import Profile from './pages/Profile'; // Fix: Ensure consistent casing
+import Profile from './pages/Profile';
+import Header from './components/Header'; // Import Header correctly
 
 export default function App() {
     return (
         <BrowserRouter>
+            <Header /> {/* Move Header outside Routes */}
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/sign-in' element={<SignIn />} />
-                <Route path='/sign-up' element={<SignUp />} /> {/* Fixed typo */}
+                <Route path='/sign-up' element={<SignUp />} />
                 <Route path='/about' element={<About />} />
                 <Route path='/profile' element={<Profile />} />
             </Routes>
